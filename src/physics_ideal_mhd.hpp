baf2d9e3c3ca8761afa3dad2e34f772d82d0b273
Chris <cwt271@nyu.edu>
2019-07-10 18:08:02 -0400

    Got rid of 4pis and added functions for each wavespeed

--- src/physics_ideal_mhd.hpp
+++ src/physics_ideal_mhd.hpp
@@ -43,12 +43,12 @@ namespace mara { struct mhd; }
 //=============================================================================
 struct mara::mhd
 {
-    using unit_conserved_density  = dimensional_value_t<-3, 1, 0, double>;
-    using unit_conserved          = dimensional_value_t< 0, 1, 0, double>;
-    using unit_flow               = dimensional_value_t< 0, 1,-1, double>;
+    using unit_conserved_density  = dimensional_value_t< -3, 1, 0, double>;
+    using unit_conserved          = dimensional_value_t<  0, 1, 0, double>;
+    using unit_flow               = dimensional_value_t<  0, 1,-1, double>;
     using unit_flux               = dimensional_value_t< -2, 1,-1, double>; //flux through faces
 
-    using conserved_density_t     = arithmetic_sequence_t<unit_conserved_density, 8>; //8 since B_x != const?
+    using conserved_density_t     = arithmetic_sequence_t<unit_conserved_density, 8>; 
     using conserved_t             = arithmetic_sequence_t<unit_conserved        , 8>;
     using flux_vector_t           = arithmetic_sequence_t<unit_flux             , 8>;
     
@@ -56,10 +56,10 @@ struct mara::mhd
 
     struct wavespeeds_t
     {
-        //TODO: NEED TO ADD EXTRA WAVE SPEEDS HERE -- which to carry around?
         unit_velocity<double> m;
         unit_velocity<double> p;
     };
+
 
     static inline primitive_t recover_primitive(
         const conserved_density_t& U,
@@ -95,7 +95,6 @@ struct mara::mhd::primitive_t : public mara::derivable_sequence_t<double, 8, pri
     const double& velocity_2()   const { return operator[](2); }
     const double& velocity_3()   const { return operator[](3); }
     const double& gas_pressure() const { return operator[](4); }
-    
     const double& bfield_1()     const { return operator[](5); }
     const double& bfield_2()     const { return operator[](6); }
     const double& bfield_3()     const { return operator[](7); }
@@ -197,7 +196,7 @@ struct mara::mhd::primitive_t : public mara::derivable_sequence_t<double, 8, pri
      /**
      * @brief      Return the square of the magnetic field magnitude.
      *
-     * @return     u^2
+     * @return     B^2
      */
     double bfield_squared() const
     {
@@ -211,7 +210,7 @@ struct mara::mhd::primitive_t : public mara::derivable_sequence_t<double, 8, pri
      *
      * @param[in]  nhat  The unit vector
      *
-     * @return     v / c
+     * @return     B /e ?   ------> need to think about and fix these units everywhere
      */
     double bfield_along(const unit_vector_t& nhat) const
     {
@@ -219,6 +218,11 @@ struct mara::mhd::primitive_t : public mara::derivable_sequence_t<double, 8, pri
         return nhat.project(_[5], _[6], _[7]);
     }
 
+     /**
+     * @brief      Return the dot product of the bfield and velocity vectors
+     *
+     * @return     B \dot v
+     */
     double bfield_dot_velocity() const
     {
         //Vector correspondence between 1 and 5; 2 and 6; 3 and 7 (e.g. vx and Bx)
@@ -243,11 +247,11 @@ struct mara::mhd::primitive_t : public mara::derivable_sequence_t<double, 8, pri
    /**
      * @brief      Return Alfven-speed squared
      *
-     * @return     B^2 / (4*pi*rho)
+     * @return     B^2 / rho
      */
     double alfven_speed_squared() const
     {
-        return bfield_squared() / (4 * M_PI * mass_density());
+        return bfield_squared() /  mass_density();
     }
 
    /**
@@ -255,12 +259,12 @@ struct mara::mhd::primitive_t : public mara::derivable_sequence_t<double, 8, pri
      *
      * @param[in]  nhat  The unit vector
      *
-     * @return     (B_nhat)^2 / (4*pi*rho)
+     * @return     (B_nhat)^2 / rho
      */
     double alfven_speed_squared_along(const unit_vector_t& nhat) const
     {
         const auto b_along = bfield_along(nhat);  
-        return b_along*b_along / (4 * M_PI * mass_density());
+        return b_along*b_along / mass_density();
     }
 
    /**
@@ -273,7 +277,7 @@ struct mara::mhd::primitive_t : public mara::derivable_sequence_t<double, 8, pri
      * @return     0.5 * ( c_s^2 + c_a^2 ) \pm 0.5*\sqrt{ (c_s^2+c_a^2)^2 - 4*c_s^2*c_{a,i}^2 }
      */
     // **** Not sure I need this. And the fast_slow thing might be a bad idea.... lol
-    double magneto_sonic_speed_square(std::string fast_slow, unit_vector_t& nhat) const
+    double magnetosonic_speed_squared(std::string fast_slow, unit_vector_t& nhat) const
     {
         const auto cs2 = sound_speed_squared();
         const auto one = cs2 + alfven_speed_squared();
@@ -311,7 +315,7 @@ struct mara::mhd::primitive_t : public mara::derivable_sequence_t<double, 8, pri
         U[1].value = d * _[1];
         U[2].value = d * _[2];
         U[3].value = d * _[3];
-        U[4].value = 0.5 * d * velocity_squared() + p / (gamma_law_index - 1);
+        U[4].value = 0.5 * d * velocity_squared() + p / (gamma_law_index - 1) /*mhd*/ + bfield_squared() / 2.0;
 
         //Bfields
         U[5].value = _[5];
@@ -358,10 +362,10 @@ struct mara::mhd::primitive_t : public mara::derivable_sequence_t<double, 8, pri
         auto bv = bfield_dot_velocity();
         auto F = flux_vector_t();
         F[0].value = v * U[0].value;
-        F[1].value = v * U[1].value + p * nhat.get_n1() /*mhd*/ - U[5].value * b / (4*M_PI); //Do I need all these 4pi's?...
-        F[2].value = v * U[2].value + p * nhat.get_n2() /*mhd*/ - U[6].value * b / (4*M_PI);
-        F[3].value = v * U[3].value + p * nhat.get_n3() /*mhd*/ - U[7].value * b / (4*M_PI); 
-        F[4].value = v * U[4].value + p * v             /*mhd*/ - b * bv   / (4*M_PI);
+        F[1].value = v * U[1].value + p * nhat.get_n1() /*mhd*/ - U[5].value * b;
+        F[2].value = v * U[2].value + p * nhat.get_n2() /*mhd*/ - U[6].value * b;
+        F[3].value = v * U[3].value + p * nhat.get_n3() /*mhd*/ - U[7].value * b; 
+        F[4].value = v * U[4].value + p * v             /*mhd*/ - bv         * b;
 
         // mhd: B \cross v
         // when flux is in direction of nhat it should become zero...
@@ -384,7 +388,7 @@ struct mara::mhd::primitive_t : public mara::derivable_sequence_t<double, 8, pri
      *
      * @return     The wavespeeds
      */
-    wavespeeds_t wavespeeds(const unit_vector_t& nhat, double gamma_law_index) const
+    wavespeeds_t sound_wave_speeds(const unit_vector_t& nhat, double gamma_law_index) const
     {
         auto cs = std::sqrt(sound_speed_squared(gamma_law_index));
         auto vn = velocity_along(nhat);
@@ -394,282 +398,55 @@ struct mara::mhd::primitive_t : public mara::derivable_sequence_t<double, 8, pri
         };
     }
 
+    wavespeeds_t alfven_wave_speeds(const unit_vector_t& nhat, double gamma_law_index) const
+    {
+        auto ca = std::sqrt( alfven_speed_squared_along(nhat) );
+        auto vn = velocity_along(nhat);
+        return{
+            make_velocity(vn - ca),
+            make_velocity(vn + ca)
+        };
+    }
+
+    wavespeeds_t fast_wave_speeds(const unit_vector_t& nhat, double gamma_law_index) const
+    {
+        auto cf = std::sqrt( magneto_sonic_speed_square("fast", nhat) );
+        auto vn = velocity_along(nhat);
+        return{
+            make_velocity(vn - cf),
+            make_velocity(vn + cf)
+        };
+    }
+
+    wavespeeds_t slow_wave_speeds(const unit_vector_t& nhat, double gamma_law_index) const
+    {
+        auto cs = std::sqrt( magneto_sonic_speed_square("slow", nhat) );
+        auto vn = velocity_along(nhat);
+        return{
+            make_velocity(vn - cs),
+            make_velocity(vn + cs)
+        };
+    }
+
     /**
      * @brief      Above function generalized to other wave speeds
      *
-     * @param[in]  nhat             The direction
-     * @param[in]  wave_function    The function returning the proper wavespeed
+     * @param[in]  nhat         The direction
+     * @param[in]  wavespeed    The wavespeed of the wave of interest
      *
      * @return     The wavespeeds
      */
-`   //
-    // *** I'm not sure I'm allowed to overload a function like this
-    // ***     or even that passing a function like that even works haha
-    // 
-    wavespeeds_t wavespeeds(const unit_vector_t& nhat, auto wave_fuction) const
+    wavespeeds_t wavespeeds_general( const unit_vector_t& nhat, double wavespeed ) const
     {
-        auto c0 = std::sqrt(wave_function);
+        auto c0 = std::sqrt(wavespeed);
         auto vn = velocity_along(nhat);
         return {
             make_velocity(vn - c0),
             make_velocity(vn + c0)
         };
     }
+    
 
-
-
-
-    /**
-     * @brief      Return the spherical source terms for gamma-law mhd
-     *
-     * @param[in]  spherical_radius   The spherical radius
-     * @param[in]  polar_angle_theta  The polar angle theta
-     * @param[in]  gamma_law_index    The gamma law index
-     *
-     * @return     Source terms in units of mass / volume / time
-     */
-    auto spherical_geometry_source_terms(
-        double spherical_radius,
-        double polar_angle_theta,
-        double gamma_law_index)
-    {
-        auto cotq = std::tan(M_PI_2 - polar_angle_theta);
-        auto vr = velocity_1();
-        auto vq = velocity_2();
-        auto vp = velocity_3();
-        auto pg = gas_pressure();
-        auto d = mass_density();
-        auto r = spherical_radius;
-        auto S = arithmetic_sequence_t<dimensional_value_t<-3, 1, -1, double>, 5>();
-        S[1].value = (2.0  * pg + d * (vq * vq        + vp * vp)) / r;
-        S[2].value = (cotq * pg + d * (vp * vp * cotq - vr * vq)) / r;
-        S[3].value =        -vp * d * (vr + vq * cotq) / r;
-        return S;
-    }
-
-
-
-
-    /**
-     * @brief      Special case of the above for 1d radial flow
-     *
-     * @param[in]  spherical_radius  The spherical radius
-     * @param[in]  gamma_law_index   The gamma law index
-     *
-     * @return     Source terms in units of mass / volume / time
-     */
-    auto spherical_geometry_source_terms_radial(double spherical_radius, double gamma_law_index)
-    {
-        auto vq = velocity_2();
-        auto pg = gas_pressure();
-        auto d = mass_density();
-        auto r = spherical_radius;
-        auto S = arithmetic_sequence_t<dimensional_value_t<-3, 1, -1, double>, 5>();
-        S[1].value = (2.0 * pg + d * vq * vq) / r;
-        return S;
-    }
-
-
-
-
-    /**
-     * @brief      Data structure that computes the left and right eigenvectors
-     *             of the flux Jacobian, and caches the variables involved to
-     *             reduce redundant calculations.
-     */
-    struct eigensystem_formulas_t
-    {
-
-
-        //=====================================================================
-        eigensystem_formulas_t(const primitive_t& p, double gamma_law_index)
-        {
-            g = gamma_law_index;
-            m = gamma_law_index - 1;
-            u = p.velocity_1();
-            v = p.velocity_2();
-            w = p.velocity_3();
-            u2 = u * u;
-            v2 = v * v;
-            w2 = w * w;
-            V2 = u2 + v2 + w2;
-            a2 = p.sound_speed_squared(gamma_law_index);
-            a = std::sqrt(a2);
-            H = 0.5 * V2 + a2 / m;
-        }
-
-
-
-
-        /**
-         * @brief      Return the Jacobian matrix dF / dU (Toro eqn. 3.79)
-         *
-         * @return     A 5x5 matrix
-         *
-         * @note       There is a typo in Toro (3rd edition) eqn. 3.79 in row 5,
-         *             column 1 of A. The expression written is 0.5 * u * ((g -
-         *             3) * H - a2), but it should read u * (0.5 * m * V2 - H)
-         *             (where m = g - 1) as it does in the 2d version in eqn.
-         *             3.70.
-         */
-        auto flux_jacobian() const
-        {
-            return matrix_t<unit_velocity<double>, 5, 5> {
-                {{0,                      1,             0,          0,         0},
-                 {m * H - u2 - a2,        (3 - g) * u,  -m * v,     -m * w,     m},
-                 {-u * v,                 v,             u,          0,         0},
-                 {-u * w,                 w,             0,          u,         0},
-                 {u * (0.5 * m * V2 - H), H - m * u2,   -m * u * v, -m * u * w, g * u}}
-            };
-        }
-
-
-
-
-        /**
-         * @brief      Return the eigenvalues of the flux Jacobian
-         *
-         * @return     The eigenvalues
-         */
-        auto eigenvalues() const
-        {
-            return mara::diagonal_matrix<unit_velocity<double>>(u - a, u, u, u, u + a);
-        }
-
-
-
-
-        /**
-         * @brief      Return the right eigenvectors of the Jacobian matrix
-         *             (Toro eqn. 3.82)
-         *
-         * @return     The eigenvectors as a 5x5 matrix
-         */
-        auto right_eigenvectors() const
-        {
-            return matrix_t<unit_scalar<double>, 5, 5> {
-                {{1,         1,        0,  0, 1},
-                 {u - a,     u,        0,  0, u + a},
-                 {v,         v,        1,  0, v},
-                 {w,         w,        0,  1, w},
-                 {H - u * a, 0.5 * V2, v,  w, H + u * a}}
-            };
-        }
-
-
-
-
-        /**
-         * @brief      Return the left eigenvectors of the Jacobian matrix (Toro
-         *             eqn. 3.83)
-         *
-         * @return     The eigenvectors as a 5x5 matrix
-         */
-        auto left_eigenvectors() const
-        {
-            return matrix_t<unit_scalar<double>, 5, 5> {
-                {{      H + (a / m) * (u - a), -(u + a / m), -v,         -w,           1, },
-                 { -2 * H + (4 / m) * a2,             2 * u,  2 * v,      2 * w,      -2, },
-                 {          -2 * v  * a2 / m,             0,  2 * a2 / m, 0,           0, },
-                 {          -2 * w  * a2 / m,             0,  0,          2 * a2 / m,  0, },
-                 {      H - (a / m) * (u + a), -(u - a / m), -v,         -w,           1, }},
-            } * (m / 2 / a2);
-        }
-
-
-
-
-        //=====================================================================
-        double g;
-        double m;
-        double u;
-        double v;
-        double w;
-        double u2;
-        double v2;
-        double w2;
-        double V2;
-        double a2;
-        double a;
-        double H;
-    };
-
-
-
-
-    /**
-     * @brief      Compute the flux Jacobian dF / dU
-     *
-     * @param[in]  gamma_law_index  The gamma law index
-     *
-     * @return     A 5x5 matrix
-     */
-    auto flux_jacobian(double gamma_law_index) const
-    {
-        return eigensystem_formulas_t(*this, gamma_law_index).flux_jacobian();
-    }
-
-
-
-
-    /**
-     * @brief      Compute the eigenvalues of the flux Jacobian
-     *
-     * @param[in]  gamma_law_index  The gamma law index
-     *
-     * @return     The eigenvalues
-     */
-    auto eigenvalues(double gamma_law_index) const
-    {
-        return eigensystem_formulas_t(*this, gamma_law_index).eigenvalues();
-    }
-
-
-
-
-    /**
-     * @brief      Compute the right eigenvectors of the flux Jacobian dF / dU
-     *
-     * @param[in]  gamma_law_index  The gamma law index
-     *
-     * @return     A 5x5 matrix
-     */
-    auto right_eigenvectors(double gamma_law_index) const
-    {
-        return eigensystem_formulas_t(*this, gamma_law_index).right_eigenvectors();
-    }
-
-
-
-
-    /**
-     * @brief      Compute the left eigenvectors of the flux Jacobian dF / dU
-     *
-     * @param[in]  gamma_law_index  The gamma law index
-     *
-     * @return     A 5x5 matrix
-     */
-    auto left_eigenvectors(double gamma_law_index) const
-    {
-        return eigensystem_formulas_t(*this, gamma_law_index).left_eigenvectors();
-    }
-
-
-
-
-    /**
-     * @brief      Compute eigenvalues and left and right eigenvectors all at
-     *             once (more efficient than calling the above functions
-     *             on-by-one).
-     *
-     * @return     A tuple (eigenvalues, right, left)
-     */
-    auto eigensystem(double gamma_law_index) const
-    {
-        auto sys = eigensystem_formulas_t(*this, gamma_law_index);
-
-        return std::make_tuple(sys.eigenvalues(), sys.right_eigenvectors(), sys.left_eigenvectors());
-    }
 };
 
 
@@ -691,6 +468,7 @@ mara::mhd::primitive_t mara::mhd::recover_primitive(
     double temperature_floor)
 {
     auto p_squared = (U[1] * U[1] + U[2] * U[2] + U[3] * U[3]).value;
+    auto b_squared = (U[5] * U[5] + U[6] * U[6] + U[7] * U[7]).value;
     auto d = U[0].value;
     auto P = primitive_t();
 
@@ -698,7 +476,10 @@ mara::mhd::primitive_t mara::mhd::recover_primitive(
     P[1] =  U[1].value / d;
     P[2] =  U[2].value / d;
     P[3] =  U[3].value / d;
-    P[4] = (U[4].value - 0.5 * p_squared / d) * (gamma_law_index - 1.0);
+    P[4] = (U[4].value - 0.5 * p_squared / d - b_squared / 2.0) * (gamma_law_index - 1.0);
+    P[5] =  U[5].value;
+    P[6] =  U[6].value;
+    P[7] =  U[7].value;
 
     if (P[4] < 0.0 && temperature_floor > 0.0)
     {
@@ -752,8 +533,8 @@ mara::mhd::flux_vector_t mara::mhd::riemann_hlle(
 {
     auto Ul = Pl.to_conserved_density(gamma_law_index);
     auto Ur = Pr.to_conserved_density(gamma_law_index);
-    auto Al = Pl.wavespeeds(nhat, gamma_law_index);
-    auto Ar = Pr.wavespeeds(nhat, gamma_law_index);
+    auto Al = Pl.fast_wave_speeds(nhat, gamma_law_index ); //This sends just a double as sound speed
+    auto Ar = Pr.fast_wave_speeds(nhat, gamma_law_index );
     auto Fl = Pl.flux(nhat, Ul);
     auto Fr = Pr.flux(nhat, Ur);
 
