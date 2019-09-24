a28404f278126208359774680ce05a0b2292c972
Chris <cwt271@nyu.edu>
2019-09-24 16:55:16 -0400

    mpi_test has first shot at logic to divvy up tree blocks amongst a given number of processes

--- examples/mpi_test.cpp
+++ examples/mpi_test.cpp
@@ -94,47 +94,48 @@ int main(int argc, char* argv[])
 {
     // Tree rank and number of children per node
     const auto tree_rank = 2;
-    const auto child_num = 1 >> tree_rank;
+    // const auto child_num = 1 >> tree_rank;
 
-    auto session = mpi::Session();
-    auto comm    = mpi::comm_world();
-    printf("Running on %d processes\n", comm.size());
-
-
-    // 1. Build trivial tree
-    auto to_zeros = [] (int value) { return mara::arithmetic_sequence_t<int, 1>{0, 0, 0, 0}; };
-    auto nullTree = mara::tree_of<tree_rank>(0).bifurcate_all(to_zeros).bifurcate_all(to_zeros).bifurcate_all(to_zeros);
+    // auto session = mpi::Session();
+    // auto comm    = mpi::comm_world();
+    // printf("Running on %d processes\n", comm.size());
 
 
-    // 2. Build tree of linear hilbert indeces
-    // TODO: This will work for a tree with unifrom refinement, but for varying refinement 
-    //       will need a routine to get the global_hindex
-    auto hilbertTree = nullTree.indexes().map([] (auto i) { return mara::hilbert_index(i); });
+    //Build topology of rank tree
+    auto to_zeros = [] (int value) { return mara::arithmetic_sequence_t<int, 4>{0, 0, 0, 0}; };
+    auto topology = mara::tree_of<tree_rank>(0).bifurcate_all(to_zeros).bifurcate_all(to_zeros);
+    auto topology_indexes = topology.indexes();
 
 
-    // 3. Assign each block a rank
-    //      this function is wrong -> use nd::divvy instead
-    auto hindex_to_rank = [] (int mpi_size, int num_blocks)
-    {
-        auto block_per_proc = std::ceil(num_blocks / mpi_size);
-        return [block_per_proc] (auto hindex) { return std::floor(hindex / block_per_proc); };
-    }; 
-    auto treesize = hilbertTree.size();
-    auto rankTree = hilbertTree.map(hindex_to_rank(comm.size(), treesize));
+    //Get hilbert indexes
+    auto hindexes = topology_indexes.map([] (auto i) { return mara::global_hilbert_index(i); });
+
+
+    //Create lists of index-hindex pairs
+    auto I = mara::linked_list_t<mara::tree_index_t<tree_rank>>{topology_indexes.begin(), topology_indexes.end()};
+    auto H = mara::linked_list_t<int>{hindexes.begin(), hindexes.end()};
+    auto hi_pair = H.pair(I);
+    
+    
+    //Sort the pair-list by hilbert index
+    auto hi_sorted = hi_pair.sort([] (auto a, auto b) { return a.first < b.first; });
     
 
-    // Make tree of empty linked lists
-    auto query_neighbors = [] (auto rankTree)
+    //Convert to nd::array and divvy into equal sized segments
+    auto size = 7;
+    auto rank_sequences = nd::make_array_from(hi_sorted) | nd::divvy(size);
+    
+
+    //Print out ragged array of each rank's assigned hilbert indexes
+    for(int i = 0; i < size; i++)
     {
-        return [rankTree] (mara::linked_list_t<int> rank_list, mara::tree_index_t<2> i)
+        std::printf("Rank %d: ", i);
+        for(int j = 0; j < rank_sequences(i).size(); j++)
         {
-            target_indices = mara::arithmetic_sequence_t<mara::tree_index_t<2>, 1>{i.prev_on(0), i.next_on(0), i.prev_on(1), i.next_on(1)};
-            return rank_list.append();
+            std::printf("%d ", rank_sequences(i)(j).first);
         }
-    };
-    auto commList = nullTree.map([] (auto i) { return mara::linked_list_t<int>{}; })
-                            .pair(nullTree.indexes())
-                            .apply(query_neighbors(rankTree));
+        std::printf("\n");
+    }
 
     return 0;
 }
