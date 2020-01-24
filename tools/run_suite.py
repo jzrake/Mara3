fd84e7a6239afe635c05b736c2a1880a5d00fca7
Jonathan Zrake <jonathan.zrake@gmail.com>
2020-01-24 13:39:16 -0500

    Fix problem with run_suite.py on last commit

--- tools/run_suite.py
+++ tools/run_suite.py
@@ -77,14 +77,17 @@ if __name__ == "__main__":
 
     for runid in suite['runs']:
 
-        suite_name           = suite.get('name', pathlib.Path(args.suite_file).stem)
-        suite_dir            = suite.get('root_dir', pathlib.Path(args.suite_file).parent)
-        mara_opts            = copy.deepcopy(suite.get('mara_opts', dict()))
-        run_dir              = os.path.join(str(suite_dir), suite_name, runid)
-        submit_file          = os.path.join(run_dir, 'submit.sh')
-        readme_file          = os.path.join(run_dir, 'README')
-        mara_opts['outdir']  = run_dir
-        mara_opts['restart'] = latest_checkpoint(run_dir) if args.restart else ''
+        suite_name          = suite.get('name', pathlib.Path(args.suite_file).stem)
+        suite_dir           = suite.get('root_dir', pathlib.Path(args.suite_file).parent)
+        mara_opts           = copy.deepcopy(suite.get('mara_opts', dict()))
+        run_dir             = os.path.join(str(suite_dir), suite_name, runid)
+        submit_file         = os.path.join(run_dir, 'submit.sh')
+        readme_file         = os.path.join(run_dir, 'README')
+        mara_opts['outdir'] = run_dir
+
+        if args.restart:
+            mara_opts['restart'] = latest_checkpoint(run_dir)
+
         mara_opts.update(suite['runs'][runid])
 
         submit_content = run_script(
